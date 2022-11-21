49df9363c22b2c9148f714c74a750946075c2ba6
patdz <patdz@ymipro.com>
2022-11-21 19:22:40 +0800

    actID and bizID

--- talkinters/model.go
+++ talkinters/model.go
@@ -4,14 +4,14 @@ import "context"
 
 type Model interface {
 	CreateTalk(ctx context.Context, talkInfo *TalkInfoW) (talkID string, err error)
-	OpenTalk(ctx context.Context, talkID string) (err error)
-	CloseTalk(ctx context.Context, talkID string) error
+	OpenTalk(ctx context.Context, actIDs, bizIDs []string, talkID string) (err error)
+	CloseTalk(ctx context.Context, actIDs, bizIDs []string, talkID string) error
 
 	AddTalkMessage(ctx context.Context, talkID string, message *TalkMessageW) (err error)
 	GetTalkMessages(ctx context.Context, talkID string, offset, count int64) (messages []*TalkMessageR, err error)
 
-	QueryTalks(ctx context.Context, creatorID, serviceID uint64, talkID string,
+	QueryTalks(ctx context.Context, actIDs, bizIDs []string, creatorID, serviceID uint64, talkID string,
 		statuses []TalkStatus) (talks []*TalkInfoR, err error)
-	GetPendingTalkInfos(ctx context.Context) ([]*TalkInfoR, error)
-	UpdateTalkServiceID(ctx context.Context, talkID string, serviceID uint64) (err error)
+	GetPendingTalkInfos(ctx context.Context, actIDs, bizIDs []string) ([]*TalkInfoR, error)
+	UpdateTalkServiceID(ctx context.Context, actIDs, bizIDs []string, talkID string, serviceID uint64) (err error)
 }
--- talkinters/talk_info.go
+++ talkinters/talk_info.go
@@ -16,6 +16,22 @@ type TalkInfoW struct {
 	CreatorID       uint64     `bson:"CreatorID" json:"creatorID" yaml:"creatorID"`
 	ServiceID       uint64     `bson:"ServiceID" json:"serviceID" yaml:"serviceID"`
 	CreatorUserName string     `bson:"CreatorUserName" json:"creatorUserName" yaml:"creatorUserName"`
+	ActID           string     `bson:"ActID" json:"ActID,omitempty" yaml:"ActID,omitempty"`
+	BizID           string     `bson:"BizID" json:"BizID,omitempty" yaml:"BizID,omitempty"`
+}
+
+func (w *TalkInfoW) Valid() (f bool) {
+	if w == nil {
+		return
+	}
+
+	if w.Title == "" || w.CreatorID == 0 || w.CreatorUserName == "" || w.ActID == "" || w.BizID == "" {
+		return
+	}
+
+	f = true
+
+	return
 }
 
 type TalkInfoR struct {
